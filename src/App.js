049cf1c3606592aa19ba4629632ffe03f1029ce6
Deepak Keshri <unodeepak1@gmail.com>
2025-03-15 11:00:19 +0530

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edita
+          Click
         </p>
         <a
           className="App-link"
